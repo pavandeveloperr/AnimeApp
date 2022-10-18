216d4f72e2999826ea49429457dd6a4248922365
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2022-10-18 22:55:04 +0530

    feat: added search feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react"
 import './Components/style.css' 
+import {AnimeList} from './Components/AnimeList'
+import {AnimeInfo} from './Components/AnimeInfo'
 
 function App() {
    
+  const [search, setSearch] = useState('Demon Slayer')
   //creating a state variable to hold response 
-  //(setAnimeData is used for updating the state & animeData is initial state )
+  //(setAnimeData is used for updating the state & animeData is our initial state )
   const [animeData, setAnimeData] = useState()
+  const [animeInfo, setAnimeInfo] = useState()
   const getData = async() => {
-  const res=await fetch(`https://api.jikan.moe/v4/anime?q=naruto&limit=20`)
+  const res=await fetch(`https://api.jikan.moe/v4/anime?q=${search}&limit=20`)
   const resData = await res.json();
   setAnimeData(resData.data)
  }
@@ -16,17 +20,38 @@ function App() {
  //it takes two arguments first is function & second is dependency
  useEffect(() => {
   getData()
- }, [])
+ }, [search] )
 
  return (
   <>
     <div className="header">
       <h1>MyAnimeList</h1>
-      <div className="search-box">
-        <input type="search" placeholder="Search your anime"/>
+    
+    <div className="search-box">
+       <input type="search" placeholder="Search your anime" onChange={(e)=>setSearch(e.target.value)}/>
+    </div>
+    </div>
+
+    <div className="container">
+      <div className="animeInfo">
+       {animeInfo && <AnimeInfo/>} 
+
+      </div>
+      
+      
+      <div className="anime-row">
+        <h2 className="text-heading">Anime</h2>
+        
+        <div className="row">
+        <AnimeList 
+        animelist={animeData}
+        setAnimeInfo={setAnimeInfo}
+        />
+        </div>
       </div>
     </div>
-  
+
+
   </>
  )
  }
