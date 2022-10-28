0e29adec24ec25c1d6c03f5e2b2cb1f2e93101f1
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2022-10-28 22:05:43 +0530

    refactor : did some changes

--- src/Components/AnimeList.js
+++ src/Components/AnimeList.js
@@ -1,24 +1,38 @@
 import React from 'react'
+import '../index.css'
+import './style.css'
 
-export const AnimeList = ({animelist}) => {
+export const AnimeList = ({animelist, setAnimeInfo}) => {
   return (
     <>
-    {/* Here we are checking the anime data is present or not */}'
+    {/* Here we are checking the anime data is present or not */}
     {/* here anime passed as an argument represents each element in the index */}
     {/* we have used map method to iterate over each element */}
 
     {
       animelist ? (
         animelist.map((anime, index)=>{
-          return(
-            <div className='card'>
-            <img src={anime.images.jpg.large_image_url} alt="animeImage"/>            <div className="anime-info">
-              <h4>{anime.title}</h4>
-            </div>
-            </div>
-          )
+          return (
+            <>
+              <div
+                className="card shadow-lg rounded-lg"
+                key={index}
+                onClick={() => setAnimeInfo(anime)}
+              >
+                <img
+                  src={anime.images.jpg.large_image_url}
+                  className="anime-img rounded-t-lg"
+                  alt="animeImage"
+                />
+                <p className='text-[8px] sm:text-[11px]'>{anime.title}</p>
+              </div>
+  
+                  
+                
+            </>
+          );
         })
-      ):"Not Found"
+      ): 'Sorry, Not Found'
     }
         
     </>
