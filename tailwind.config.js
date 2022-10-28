aaba48d1cf9e79450acbea7649254a6ce705c83d
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2022-10-28 22:03:31 +0530

    chore : iniciated packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,25 +5,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-      screens: {
-        sm: "640px",
-        // => @media (min-width: 640px) { ... }
-  
-        md: "768px",
-        // => @media (min-width: 768px) { ... }
-  
-        lg: "992px",
-        // => @media (min-width: 992px) { ... }
-  
-        xl: "1024px",
-        // => @media (min-width: 1024px) { ... }
-  
-        "2xl": "1280px",
-        // => @media (min-width: 1280px) { ... }
-  
-        "3xl": "1536px",
-        // => @media (min-width: 1536px) { ... }
-      },
       fontFamily: {
         body: ['Josefin Sans']
       }
