8a8d599f1dac3e3ab1615ce10a5fd2ae25a2d2ea
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2022-10-28 22:02:20 +0530

    feat : created search

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './index.css'
+import './Components/style.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
 	faMagnifyingGlass
 } from "@fortawesome/free-solid-svg-icons";
+import { AnimeList } from './Components/AnimeList';
+
+
 function App() {
-
-
+  const [search, setSearch] = useState('Naruto')
+  const [animeData, setAnimeData] = useState()
   const getData = async () => {
-    const res = await fetch(`https://api.jikan.moe/v4/anime?q=naruto&limit=20`)
+    const res = await fetch(`https://api.jikan.moe/v4/anime?q=${search}&limit=20`)
     const resData = await res.json();
-    console.log(resData)
+    setAnimeData(resData.data)
   }
 
   //this method takes two args first is function and sec is dependency
   useEffect(() => {
-    getData()
-  }, [])
+  getData()
+  },[search])
+
   return (
     <>
       <nav className="dark:bg-slate-800 navbar bg-gray-100 w-full top-0 left-0 z-[5] sm:py-0">
         <div className="nav-wrapper w-full mx-auto flex flex-row items-center justify-between max-w-[1280px]">
           <div className="max-w-8xl px-4 py-1 sm:px-6 lg:px-8 w-full mx-auto">
             <div className="relative flex items-center justify-between h-16">
-              <h3 className="md:block font-body font-semibold text-3xl text-sky-400 logo-text">
-                MyAnime
+              <h3 className="md:block font-body text-3xl logo-text">
+                My<strong>Anime</strong>List
               </h3>
-              {/* </Link>
-						{isAuth ? (
-							<div
-								className="search-form hidden sm:block relative"
-								ref={searchListReference}
-							> */}
-              <form className='sm: pl-2.5'>
+              <form className="sm: pl-2.5">
                 <div className="relative rounded-sm shadow-sm bg-gray-200">
                   <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                     <span className="text-gray-400 sm:text-sm">
@@ -45,10 +44,10 @@ function App() {
                     id="searchTextDesktop"
                     required
                     // value={searchQueryText}
-                    autoComplete="off"
-                    // onChange={handleSearchTextChange}
-                    className="focus:ring-sky-500 focus:border-sky-500 block w-full pl-8 bg-transparent pr-2 py-1.5 sm:text-base mr-1  text-slate-900 border-gray-300 rounded-sm outline-none"
-                    placeholder="Search Animes..."
+                    onChange={(e) => setSearch(e.target.value)}
+                    className="shadow-sm focus-outlet-none focus:border-orange-600 focus:ring-black-500 focus:ring-2 invalid:bg-gray-200
+                     sm:text-sm block w-full pl-8 pr-2 py-1.5 sm:text-sm mr-1 font-body border-gray-300 rounded-sm outline-none sm:items-center"
+                    placeholder="Search your Anime.."
                   />
                 </div>
               </form>
@@ -56,6 +55,22 @@ function App() {
           </div>
         </div>
       </nav>
+
+      <div className="contaier">
+        <div className="animeInfo"></div>
+        <div clasisName="anime-row">
+          {/* <h2 className="text-heading">anime</h2> */}
+          <div className="grid grid-cols-2 lg:grid-cols-2 md:grid-cols-4 sm:grid-cols-3 gap-4 p-4 text-red-500 text-center bg-gray-100">
+            <AnimeList animelist={animeData} />
+          </div>
+        </div>
+      </div>
+      <footer class="bg-white-200 text-center text-black-400 lg:text-left">
+        <div className='my-4'>
+        <h3 className='footer-content'>Made with <i className="fas fa-heart" aria-hidden="true"></i> <a class="link" href="https://github.com/pavandeveloperr">
+             Pavan Kulkarni </a></h3> 
+          </div>      
+      </footer>
     </>
   );
 }
